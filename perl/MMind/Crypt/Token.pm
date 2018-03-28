cba31f17efa4beb23d795612c95d9f97121aefb9
Ankit Pati <contact@ankitpati.in>
2018-03-28 09:08:42 +0530

    handle JWT tokens

---
+++ perl/MMind/Crypt/Token.pm
@@ -0,0 +1,25 @@
+package MMind::Crypt::Token;
+
+use strict;
+use warnings;
+
+use base qw(Exporter);
+
+use Crypt::JWT qw(encode_jwt decode_jwt);
+
+use MMind::Config qw(getconfig);
+
+our EXPORT_OK = qw(get_token get_payload);
+
+my $alg = getconfig 'token_algorithm';
+my $key = getconfig 'token_secret_symmetric';
+
+sub get_token {
+    return encode_jwt payload => shift, alg => $alg, key => $key;
+}
+
+sub get_payload {
+    return decode_jwt token => shift, accepted_alg => $alg, key => $key;
+}
+
+1;
