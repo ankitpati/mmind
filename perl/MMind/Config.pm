7f4ebbcb1e7072e5fa36b6ae79564b2741b27a6c
Ankit Pati <contact@ankitpati.in>
2018-03-28 09:17:35 +0530

    FIX: all -> All, remove Primary key as it is set by default

--- perl/MMind/Config.pm
+++ perl/MMind/Config.pm
@@ -8,8 +8,7 @@ use base qw(Exporter MMind::DBI);
 our @EXPORT_OK = qw(getconfig);
 
 __PACKAGE__->table ('config');
-__PACKAGE__->columns (all => qw(key val));
-__PACKAGE__->columns (Primary => qw(key));
+__PACKAGE__->columns (All => qw(key val));
 
 our %config;
 
