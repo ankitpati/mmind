a6b6ccd14152d8736ae915a96e58be1ed13faa7a
Ankit Pati <contact@ankitpati.in>
2018-03-29 22:18:52 +0530

    add and enforce `issued at` and `relative expiry` claims

--- perl/MMind/Crypt/Token.pm
+++ perl/MMind/Crypt/Token.pm
@@ -11,14 +11,18 @@ use MMind::Config qw(getconfig);
 
 our @EXPORT_OK = qw(get_token get_payload);
 
-my ($alg, $key) = getconfig 'token_algorithm', 'token_secret_symmetric';
+my ($alg, $key, $relative_exp) = getconfig 'token_algorithm',
+                                           'token_secret_symmetric',
+                                           'token_relative_expiry_seconds';
 
 sub get_token {
-    return encode_jwt payload => shift, alg => $alg, key => $key;
+    return encode_jwt payload => shift, alg => $alg, key => $key,
+                      auto_iat => 1, relative_exp => $relative_exp;
 }
 
 sub get_payload {
-    return decode_jwt token => shift, accepted_alg => $alg, key => $key;
+    return decode_jwt token => shift, accepted_alg => $alg, key => $key,
+                      verify_iat => 1, verify_exp => 1;
 }
 
 1;
