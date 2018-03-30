56a554f67e0350aad191cf423062819639ee52bd
Ankit Pati <contact@ankitpati.in>
2018-03-30 16:59:36 +0530

    Use JWE instead of JWT
     • JWT was exposing salted, bcrypt-hashed passwords to potential attackers.
     • Therefore, switch to JWE so the entire token is encrypted.

--- perl/MMind/Crypt/Token.pm
+++ perl/MMind/Crypt/Token.pm
@@ -11,18 +11,20 @@ use MMind::Config qw(getconfig);
 
 our @EXPORT_OK = qw(get_token get_payload);
 
-my ($alg, $key, $relative_exp) = getconfig 'token_algorithm',
-                                           'token_secret_symmetric',
-                                           'token_relative_expiry_seconds';
+my ($alg, $key, $enc, $relative_exp) = getconfig qw(
+    token_algorithm token_secret_symmetric token_encryption
+    token_relative_expiry_seconds
+);
 
 sub get_token {
-    return encode_jwt payload => shift, alg => $alg, key => $key,
+    return encode_jwt payload => shift, alg => $alg,
+                      key => { kty => 'oct', k => $key }, enc => $enc,
                       auto_iat => 1, relative_exp => $relative_exp;
 }
 
 sub get_payload {
     return decode_jwt token => shift, accepted_alg => $alg, key => $key,
-                      verify_iat => 1, verify_exp => 1;
+                      enc => $enc, verify_iat => 1, verify_exp => 1;
 }
 
 1;
