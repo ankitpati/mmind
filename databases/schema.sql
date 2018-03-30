e4be5ae7c41e373130b0230a03a6118ac4d78f0b
Ankit Pati <contact@ankitpati.in>
2018-03-30 19:32:50 +0530

    update schema for responses, and associated tables

--- databases/schema.sql
+++ databases/schema.sql
@@ -24,52 +24,66 @@ create table users (
 );
 
 create table responses (
-    id    integer primary key auto_increment,
-    total integer not null
+    user_id  integer not null,
+    month    tinyint not null,
+    turnover integer not null,
+    is_ack   boolean not null,
+
+    primary key (user_id, month),
+    foreign key (user_id) references users(id) on delete cascade
 );
 
 create table response_lines (
     response_line_id integer  primary key auto_increment,
-    response_id      integer  not null,
+    user_id          integer  not null,
+    month            tinyint  not null,
     hsn_code         char(20) not null,
     sales            integer  not null,
     purchase         integer  not null,
     trade_margin     integer  not null,
 
-    foreign key (response_id) references responses(id) on delete cascade
+    foreign key (user_id, month) references responses(user_id, month)
+        on delete cascade
 );
 
 create table received_texts (
     id          integer  primary key auto_increment,
-    user_id     integer  not null,
     phone       char(20) not null,
     recwhen     datetime not null,
     body        text     not null,
-    response_id integer, /* foreign key, but used only for joining */
+
+    /* foreign key (user_id, month), but used only for joining */
+    user_id     integer  not null,
+    month       tinyint  not null,
 
     foreign key (user_id) references users(id) on delete cascade
 );
 
 create table received_emails (
     id          integer   primary key auto_increment,
-    user_id     integer   not null,
     email       char(100) not null,
     recwhen     datetime  not null,
     body        text      not null,
-    response_id integer, /* foreign key, but used only for joining */
+
+    /* foreign key (user_id, month), but used only for joining */
+    user_id     integer   not null,
+    month       tinyint   not null,
 
     foreign key (user_id) references users(id) on delete cascade
 );
 
 create table received_posts (
     id          integer  primary key auto_increment,
-    user_id     integer  not null,
     ip_address  char(50) not null, /* for IPv6 */
     recwhen     datetime not null,
-    response_id integer  not null,
 
-    foreign key (user_id)     references users(id)     on delete cascade,
-    foreign key (response_id) references responses(id) on delete cascade
+    user_id     integer   not null,
+    month       tinyint   not null,
+
+    foreign key (user_id)        references users(id)
+        on delete cascade,
+    foreign key (user_id, month) references responses(user_id, month)
+        on delete cascade
 );
 
 create table language (
