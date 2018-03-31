9fa3e70c1817d875511d442923fb521c59c755e7
Ankit Pati <contact@ankitpati.in>
2018-03-31 07:06:48 +0530

    minor fix, remove unused imports, update schema

--- perl/MMind/Responses.pm
+++ perl/MMind/Responses.pm
@@ -6,16 +6,15 @@ use warnings;
 use base qw(MMind::DBI);
 
 use Carp;
-use MMind::Util::Validate qw(month);
-use MMind::Util::Normalise qw(normalise_phone normalise_email
-                              normalise_password);
+use MMind::Util::Validate qw(validate_month validate_year);
 use MMind::ResponseLines;
 
 __PACKAGE__->table ('responses');
-__PACKAGE__->columns (All => qw(user_id month turnover is_ack));
-__PACKAGE__->columns (Primary => qw(user_id month));
+__PACKAGE__->columns (All => qw(user_id month year turnover is_ack));
+__PACKAGE__->columns (Primary => qw(user_id month year));
 __PACKAGE__->has_many (response_lines => qw(MMind::ResponseLines));
 __PACKAGE__->constrain_column (month => sub { validate_month $_ });
+__PACKAGE__->constrain_column (year => sub { validate_year $_ });
 
 sub new {
     my $class = shift;
@@ -23,17 +22,18 @@ sub new {
     local $class->db_Main->{AutoCommit};
         # turn off auto-commit until this goes out of scope
 
-    my ($user_id, $month) = (shift, shift);
+    my ($user_id, $month, $year) = (shift, shift, shift);
 
     my $ret = __PACKAGE__->insert ({
         user_id  => $user_id,
         month    => $month,
+        year     => $year,
         turnover => shift,
         is_ack   => shift,
     });
 
     eval {
-        new MMind::ResponseLines ($user_id, $month, @$_) foreach @_;
+        new MMind::ResponseLines ($user_id, $month, $year, @$_) foreach @_;
     };
     if ($@) {
         $class->dbi_rollback;
