86495d858913a4becf6fae5f30d618a2a279c76d
Ankit Pati <contact@ankitpati.in>
2018-03-31 08:16:39 +0530

    factor out unblessing of individual Class::DBI objects

--- perl/MMind/Util/Unbless.pm
+++ perl/MMind/Util/Unbless.pm
@@ -7,18 +7,21 @@ use base qw(Exporter);
 
 our @EXPORT_OK = qw(retrieve_all_unbless);
 
+sub dbi_unbless {
+    my $obj = shift;
+    my %unblessed;
+    $unblessed{$_} = $obj->$_ foreach $obj->columns;
+    return %unblessed;
+}
+
 sub retrieve_all_unbless {
     my $dbi_class = shift;
 
     my @dbi_objects = $dbi_class->retrieve_all;
-    my @dbi_columns = $dbi_class->columns;
 
     my @obj_array_unbless;
-    foreach my $obj (@dbi_objects) {
-        my %obj_hash_unbless;
-        $obj_hash_unbless{$_} = $obj->$_ foreach @dbi_columns;
-        push @obj_array_unbless, \%obj_hash_unbless;
-    }
+    push @obj_array_unbless, { dbi_unbless $_ }
+        foreach $dbi_class->retrieve_all;
 
     return @obj_array_unbless;
 }
